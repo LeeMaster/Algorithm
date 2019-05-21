581049723c58a4fa7fa0ae02f9715e73dab5089c
leemaster <leemast3r@outlook.com>
2019-05-22 00:59:48 +0800

    add the document

--- thinking/src/lib.rs
+++ thinking/src/lib.rs
@@ -1,3 +1,13 @@
+//! # 挑战程序设计竞赛第一本书
+//! 
+//! 整个代码仓库使用Rust实现了书中全部的算法，并做了一些解题思路的注解
+//! * prepare 模块 第一章节
+//! * primary 模块 第二章节
+//! * intermediate 模块 第四章节
+//! * advanced 模块 第四章节
+
+
+
 pub mod prepare;
 pub mod primary;
 pub mod advanced;
--- thinking/src/prepare/triangle.rs
+++ thinking/src/prepare/triangle.rs
@@ -1,5 +1,13 @@
+//! # 最长三角形周长问题
+//! 
+//! 给定一个数组，从中寻找三个边，使其能组成的三角形周长最长，并输出周长
+
 use std::cmp::max;
 
+/// ## 暴力解法
+/// 思考如何寻找一个三角形，如果需要三个边，那么对一个数组的处理自然从最开始的三个数开始迭代，
+/// 如果前三个数不能组成三角形，那么换一个边继续迭代，3次方的解法
+/// 
 // the violence loop to solve the problem 
 #[allow(dead_code)]
 pub fn max_triangle_perimeter_violence(array:& Vec<i32>) -> i32 {
@@ -21,6 +29,8 @@ pub fn max_triangle_perimeter_violence(array:& Vec<i32>) -> i32 {
     ans
 }
 
+/// ## 优化的解法
+/// 先排序，从最大边开始迭代，第一个构成的三角形自然就是最大的三角形
 // nlogn solution
 #[allow(dead_code)]
 pub fn max_triangle_perimeter_nlogn(array:& Vec<i32>) -> i32 {
