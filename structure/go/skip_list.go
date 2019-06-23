519ab3610f28a0154b752cd3fe25f394fa5486e0
leemaster <leemast3r@outlook.com>
2019-06-23 23:44:29 +0800

    update the skip list

--- structure/go/skip_list.go
+++ structure/go/skip_list.go
@@ -4,36 +4,52 @@ import (
 	"math/rand"
 )
 
+type SkipListEntry struct {
+	Element Order 
+	Index Index
+}
+
+func (entry *SkipListEntry) Cmp(other Order) int{
+	return entry.Element.Cmp(other)
+}
+
+func (entry *SkipListEntry) Key() int{
+	return entry.Index.Key()
+}
+
 type SkipNode struct {
 	Element Entry 
-	RightNode *SkipNode
-	BottomNode *SkipNode
+	Left *SkipNode
+	Right *SkipNode
+	Top *SkipNode
+	Bottom *SkipNode
 }
 
 type SkipList struct {
-	Top *SkipNode
+	Head *SkipNode
 	Tail *SkipNode
 	Level int
 	Size int 
 }
 
+
 // generate a node for skiplist 
 func newSkipNode(element Entry) SkipNode{
 	return SkipNode{
 		Element: element,
-		RightNode: nil,
-		BottomNode: nil,
 	}
 }
 
 func BuildSkipList(list []Entry) SkipList{
-	node := newSkipNode(list[0])
+	node := newSkipNode(nil)
+	// use the sentry head 
 	res := SkipList{
 		&node,
 		&node,
-		0,
+		1, // default level is 1 
 		0,
 	}
+
 
 	// for _,entry := range list[1:] {
 	// 	node := newSkipNode(entry,entry)
@@ -44,10 +60,53 @@ func BuildSkipList(list []Entry) SkipList{
 	return res 
 }
 
+func (list *SkipList) findPreviousNode(node *SkipNode) *SkipNode{
+	if list.Size == 0 {
+		return list.Head
+	}else{
+		cusor := list.Head
+		// to find the bottom linked list head 
+		for list.Head.Bottom != nil {
+			cusor = cusor.Bottom
+		}
+
+		cusor = cusor.Right
+
+		// find the suit position 
+		for cusor.Right.Element.Cmp(node.Element) <= 0{
+			cusor = cusor.Right
+		}
+
+		return cusor
+	}
+}
+
 // from the entry to build the skip list 
 func (list *SkipList) insert(entry Entry) error {
+
+	currentNode := newSkipNode(entry)
+
+	conactNodeFunc := func(pre *SkipNode,cur *SkipNode){
+		tempNode := pre.Right
+		pre.Right = cur
+		cur.Left = cur
+		cur.Right = tempNode
+		tempNode.Left = cur
+	}
+
+	conactNodeFunc(list.findPreviousNode(&currentNode),&currentNode)
+
+	// TODO  use the random to generate the level and then do the build for level 
+
+	// buildLevelFunc := func
+
+	//level := list.random(entry.Key())
+
+
 	return nil 
 }
+
+
 
 // cal the random level 
 func (list *SkipList) random(code int) int {
@@ -66,7 +125,15 @@ func (list *SkipList) Get(key Index) (Order,error){
 }
 
 func (list *SkipList) Put(key Index,value Order) bool{
-	return false
+	entry := SkipListEntry{
+		value,
+		key,
+	}
+
+	// this usage in golang is that accessing the address to get the real object for interface 
+	err := list.insert(&entry)
+
+	return err == nil
 }
 
 func (list *SkipList) Remove(Key Index) bool{
