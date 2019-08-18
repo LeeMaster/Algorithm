9f650c3c03a1b69f63b8c993604801e5c68607f0
leemaster <leemast3r@outlook.com>
2019-08-18 12:36:57 +0800

    add Cmake file and will complete on Linux Vm

---
+++ daily/leetcode/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * leetcode 题解
+ * cmake build   
+ * leet {problem} run 
+ * output   
+ * input  
+ * result of the algoritm 
+ */
+
+#include<iostream>
+#include<map>
+
+using namespace std;
+
+int main(int argc,char ** argv){
+
+  return 0;
+}
